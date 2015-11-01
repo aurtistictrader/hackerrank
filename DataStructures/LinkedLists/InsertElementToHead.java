155c9e7784c6c3f0d31814d8a8aa843b6c3b4dcd
Cheng Peng <cwpeng@uwaterloo.ca>
2015-11-01 02:34:12 -0500

    Added solution to add element to head

---
+++ DataStructures/LinkedLists/InsertElementToHead.java
@@ -0,0 +1,23 @@
+/*
+  Insert Node at the beginning of a linked list 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+// This is a "method-only" submission. 
+// You only need to complete this method. 
+
+Node Insert(Node head,int x) {
+    if (head == null) {
+        head.data = x;
+        return head;
+    } else {
+        Node newHead = new Node();
+        newHead.data = x;
+        newHead.next = head;
+        return newHead;
+    }
+}
