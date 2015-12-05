b30a6be19a918ffb50e5a81126395659be07ddf2
Cheng Peng <cwpeng@uwaterloo.ca>
2015-12-05 12:51:26 -0500

    Added service lane soln

---
+++ Algorithms/Implementation/ServiceLane.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int t = in.nextInt();
+        int width[] = new int[n];
+        for(int width_i=0; width_i < n; width_i++){
+            width[width_i] = in.nextInt();
+        }
+        for(int a0 = 0; a0 < t; a0++){
+            int i = in.nextInt();
+            int j = in.nextInt();
+            
+            int least = 100000;
+            for (int current = i; current <= j; current++) {
+                if (width[current] < least) {
+                    least = width[current];
+                }
+            }
+            
+            System.out.println(least);
+        }
+    }
+}
